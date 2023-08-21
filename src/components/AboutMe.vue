4b0c48a656f36f83d92f7da76cdb74f0fc553a6d
Matheus Cavalcante <matheuscavalcs@gmail.com>
2023-08-21 09:45:23 -0300

    component

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,47 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="bg-white text-black">
+        <div class="px-14 py-10 md:mx-32 pb-20">
+            <p class="text-4xl font-bold">Sobre mim</p>
+            <div class="mt-3">
+                <p>Profissional ágil, com aprendizado contínuo, comunicativo, habituado com a rotina de uma empresa e
+                    sempre
+                    procurando resolver problemas com soluções rápidas e práticas. Procuro sempre me manter atualizado sobre
+                    as
+                    tecnologias que uso e, assim, me tornar um dev o mais completo possível. </p>
+            </div>
+            <div class="mt-3">
+                <p>
+                    Sou um desenvolvedor web apaixonado com experiência sólida nas
+                    tecnologias mais recentes. Minhas habilidades abrangem desde a criação de interfaces atraentes com <span
+                        class="underline font-bold">HTML5</span>,
+                    <span class="underline font-bold">CSS3</span>, e <span class="underline font-bold">JavaScript
+                        (ES12)</span>,
+                    até o
+                    desenvolvimento de aplicativos robustos com <span class="underline font-bold">PHP (7.x a 8.x)</span> e o
+                    uso
+                    de
+                    poderosos frameworks como <span class="underline font-bold">Laravel (7.x a 10.x)</span>. Com uma
+                    abordagem
+                    centrada no usuário, utilizo tanto a stack
+                    <span class="underline font-bold">Vue.js 3</span>, <span class="underline font-bold">InertiaJs</span> e
+                    <span class="underline font-bold">TailwindCSS</span>
+                    quanto a <span class="underline font-bold">Livewire (2 e 3)</span>, <span
+                        class="underline font-bold">Alpine.js 3</span> e
+                    <span class="underline font-bold">TailwindCSS</span> (Tall Stack) para criar experiências interativas e
+                    dinâmicas me sentindo completamente confortavel com ambas. Tenho conhecimento nos bancos de
+                    dados <span class="underline font-bold">MariaDB</span> e <span
+                        class="underline font-bold">PostgreSQL</span>. Além disso,
+                    possuo proficiência em
+                    ferramentas essenciais como <span class="underline font-bold">Git</span> e <span
+                        class="underline font-bold">Docker</span>
+                    para gerenciar projetos de forma eficiente. Estou sempre em busca
+                    de novos desafios e ansioso para transformar ideias em realidade.
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/components/MyProjects.vue
@@ -0,0 +1,59 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="bg-white text-black">
+        <div class="px-14 py-10 md:mx-32">
+            <p class="text-4xl font-bold">Meus Projetos</p>
+
+            <div class="flex gap-3 mt-5">
+                <div>
+                    <img class="object-cover w-[650px] h-96"
+                        src="https://i.pinimg.com/originals/48/28/46/482846450ac995ca29d21134824b935f.jpg" alt="">
+                </div>
+
+                <div class="">
+                    <div class="w-80">
+                        <h1 class="text-center text-3xl font-bold">Food App</h1>
+
+                        <p class="mt-2">
+                            Food App e um aplicativo de refeicao e delivery completo onde e possivel escolher varias opcoes
+                            entre diferentes categorias. Conta com todo um painel de administracao
+                        </p>
+                    </div>
+
+                    <div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="px-14 py-10 md:mx-32">
+            <p class="text-4xl font-bold">Meus Projetos</p>
+
+            <div class="flex gap-3 mt-5">
+                <div>
+                    <img class="object-cover w-[650px] h-96"
+                        src="https://i.pinimg.com/originals/48/28/46/482846450ac995ca29d21134824b935f.jpg" alt="">
+                </div>
+
+                <div class="">
+                    <div class="w-80">
+                        <h1 class="text-center text-3xl font-bold">Food App</h1>
+
+                        <p class="mt-2">
+                            Food App e um aplicativo de refeicao e delivery completo onde e possivel escolher varias opcoes
+                            entre diferentes categorias. Conta com todo um painel de administracao
+                        </p>
+                    </div>
+
+                    <div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
