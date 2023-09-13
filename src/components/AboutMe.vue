d2cfb0fb9de3f3f701f2850d348720d45f20b4fb
Matheus Cavalcante <matheuscavalcs@gmail.com>
2023-09-12 21:38:20 -0300

    update about me

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -16,31 +16,28 @@
             </div>
             <div class="mt-3">
                 <p>
-                    Sou um desenvolvedor web apaixonado com experiência sólida nas
-                    tecnologias mais recentes. Minhas habilidades abrangem desde a criação de interfaces atraentes com <span
-                        class="underline font-bold">HTML5</span>,
-                    <span class="underline font-bold">CSS3</span>, e <span class="underline font-bold">JavaScript
-                        (ES12)</span>,
-                    até o
-                    desenvolvimento de aplicativos robustos com <span class="underline font-bold">PHP (7.x a 8.x)</span> e o
-                    uso
-                    de
-                    poderosos frameworks como <span class="underline font-bold">Laravel (7.x a 10.x)</span>. Com uma
-                    abordagem
-                    centrada no usuário, utilizo tanto a stack
-                    <span class="underline font-bold">Vue.js 3</span>, <span class="underline font-bold">InertiaJs</span> e
-                    <span class="underline font-bold">TailwindCSS</span>
-                    quanto a <span class="underline font-bold">Livewire (2 e 3)</span>, <span
-                        class="underline font-bold">Alpine.js 3</span> e
-                    <span class="underline font-bold">TailwindCSS</span> (Tall Stack) para criar experiências interativas e
-                    dinâmicas me sentindo completamente confortável com ambas. Tenho conhecimento nos bancos de
-                    dados <span class="underline font-bold">MariaDB</span> e <span
-                        class="underline font-bold">PostgreSQL</span>. Além disso,
-                    possuo proficiência em
-                    ferramentas essenciais como <span class="underline font-bold">Git</span> e <span
-                        class="underline font-bold">Docker</span>
-                    para gerenciar projetos de forma eficiente. Estou sempre em busca
-                    de novos desafios e ansioso para transformar ideias em realidade.
+                    Sou um desenvolvedor web apaixonado com uma sólida experiência nas mais recentes tecnologias. Minhas
+                    habilidades abrangem desde a criação de interfaces atraentes com <span
+                        class="underline font-bold">HTML5</span>, <span class="underline font-bold">CSS3</span> e <span
+                        class="underline font-bold">JavaScript
+                        (ES12)</span>, até o
+                    desenvolvimento de aplicativos robustos com <span class="underline font-bold">PHP (7.x a 8.x)</span> e a
+                    utilização de poderosos frameworks como
+                    <span class="underline font-bold">Laravel (7.x a 10.x)</span>. Com uma abordagem centrada no usuário,
+                    utilizo tanto a stack <span class="underline font-bold">Vue.js 3</span>, <span
+                        class="underline font-bold">InertiaJs</span> e
+                    <span class="underline font-bold">TailwindCSS</span> quanto o <span class="underline font-bold">Livewire
+                        (2 e 3)</span>, <span class="underline font-bold">Alpine.js 3</span> e <span
+                        class="underline font-bold">TailwindCSS</span> (Tall Stack) para criar experiências
+                    interativas e dinâmicas, sentindo-me completamente à vontade com ambas. Além disso, falando da Tall
+                    Stack, tenho conhecimento em desenvolvimento de painéis de administração com <span
+                        class="underline font-bold">FilamentPHP 3</span>. Também tenho
+                    experiência em bancos de dados <span class="underline font-bold">MySQL</span> e <span
+                        class="underline font-bold">PostgreSQL</span>. Além disso, possuo proficiência em ferramentas
+                    essenciais como <span class="underline font-bold">Git</span> e <span
+                        class="underline font-bold">Docker</span> para gerenciar projetos de forma eficiente. Estou sempre
+                    em busca de novos
+                    desafios e ansioso para transformar ideias em realidade.
                 </p>
             </div>
         </div>
